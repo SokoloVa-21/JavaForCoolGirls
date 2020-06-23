f8c0811f94ceae9b03040499926be0859d963d06
SokoloVa-21 <55749776+SokoloVa-21@users.noreply.github.com>
2020-06-23 10:10:38 +0300

    Add files via upload

---
+++ mylab2/исходники/Main.java
@@ -0,0 +1,22 @@
+package ru.spbstu.main;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape[] shapes = null;
+
+        /*
+         * TODO: Выполнить действия над массивом 'shapes'
+         *
+         * 1. Проинициализировать переменную 'shapes' массивом
+         *    содержащим 10 произвольных фигур. Массив должен
+         *    содержать экземпляры классов Circle, Rectangle
+         *    и Triangle.
+         *
+         * 2. Найти в массиве 'shapes' фигуру с максимальной
+         *    площадью. Для поиска фигуры необходимо создать
+         *    статический метод в текущем классе (Main).
+         */
+    }
+}
